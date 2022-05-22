6d807ebcbb8cf07779fdbbf933126ed42d2ced64
whosanelll <khamzaanel3@gmail.com>
2022-05-22 22:12:21 +0600

    updated js code

---
+++ index.js
@@ -0,0 +1,22 @@
+var btn = document.getElementById("btn");
+
+
+function key(){
+    alert("Search function is not available now!");
+}
+
+function qwerty(){
+    alert("This page will be acceptable in soon");
+}
+
+function change_btn_text(){
+    btn.innerHTML = "sashsahshadsahds";
+}
+
+function change_btn_text2(){
+    btn.innerHTML = "Hire me";
+}
+
+function music() {
+    document.getElementById('sound').play();
+}
